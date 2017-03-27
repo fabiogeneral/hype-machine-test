e7f6ba09f2c51a0b7366c69efa8e4356c1a0b32f
fabiogeneral <fabiogeneral@gmail.com>
2017-03-26 21:05:24 -0400

    fixed API paths

--- js/services/request.js
+++ js/services/request.js
@@ -3,13 +3,13 @@ angular.module('HypeMachine')
 .factory('Request', function RequestFactory($http) {
 	return {
 		latest: function() {
-			return $http({method: 'GET', url: 'https://api.hypem.com/playlist/loved/lun0/json/1'}); // request-latest
-		},																
+			return $http({method: 'GET', url: 'https://api.hypem.com/playlist/history/lun0/json/1'}); // request-latest
+		},																// supposing that the request was about the last tracks heard by the user - listening history
 		loved: function() {
-			return $http({method: 'GET', url: 'https://api.hypem.com/playlist/obsessed/lun0/json/1'}); // request-loved
-		},																
+			return $http({method: 'GET', url: 'https://api.hypem.com/playlist/loved/lun0/json/1'}); // request-loved
+		},																// Supposing that the request are those marked by the user as loved - favorites
 		posted: function() {
 			return $http({method: 'GET', url: 'https://api.hypem.com/playlist/feed/lun0/json/1'}); // request-posted
-		}
+		}																	// Supposing that the request was about the user feed
 	};
 });
