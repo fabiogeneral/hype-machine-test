adfe008b49b7dc47d6a3cac8fd1eb634d2682a25
fabiogeneral <fabiogeneral@gmail.com>
2017-03-27 09:03:27 -0400

    tracks looped through API pages

--- js/controllers/request-latest-controller.js
+++ js/controllers/request-latest-controller.js
@@ -1,18 +1,27 @@
 // Controller to Latest Section
 angular.module('HypeMachine')
-.controller('RequestLatestController', function($scope, Request) {
-  Request.latest()
-  .then(function(data) {
-    // Passing API object to array
-    var inputObj = data.data; // object
-		var output = []; // array
-		for (var key in inputObj) {
-			// Taking out the "version" Object key of API page
-		  if (key < 40) { // length of each API page
-		  	output.push(inputObj[key]); // put inside of array
-		  }
-		}
-		// Requests
-		$scope.requests = output;
-  });
+.controller('RequestLatestController', function($scope, $http) {
+	var output = []; // empty array
+	for (var pages = 1; pages < 5; pages++) { // range of API pages (.length?)
+		//I limited the number of pages because if I request the API link many times, it drops the server
+	  $http.get('https://api.hypem.com/playlist/history/lun0/json/' + pages)
+	  // supposing that the request was about the last tracks heard by the user - listening history
+	  .then(function(data) {
+	    // Passing API object to array
+	    var inputObj = data.data; // object
+	    if (i == undefined) {
+	    	var i = 0; // just the 1st time
+	    }
+			for (var key in inputObj) {
+				// Taking out the "version" Object key of API page
+			  if (key < 40) { // length of each API page
+			  	output.push(inputObj[i]); // put inside of array
+			  }
+			  i++; // next index
+			}
+			// Requests
+			$scope.requests = output;
+			//console.log($scope.requests);
+	  });
+	}
 });
--- js/controllers/request-posted-controller.js
+++ js/controllers/request-posted-controller.js
@@ -1,25 +1,32 @@
 // Controller to Posted Section
 angular.module('HypeMachine')
-.controller('RequestPostedController', function($scope, Request) {
-  Request.posted()
-  .then(function(data) {
-    //$scope.requests = data.data;
-    // Passing API object to array
-    var inputObj = data.data; // object
-		var output = []; // array
-		for (var key in inputObj) {
-			// Taking out the "version" Object key of API page
-		  if (key < 40) { // length of each API page
-		  	output.push(inputObj[key]); // put inside of array
-		  }
-		}
-		// Requests
-		$scope.requests = output;
-		//consoling
-		console.log('Input ---->', data.data);
-		console.log('===========');
-		console.log('Array ---->', output);
-		console.log('===========');
-    console.log('Each Object ---->', output[0]);
-  });
+.controller('RequestPostedController', function($scope, $http) {
+	var output = []; // empty array
+	for (var pages = 1; pages < 5; pages++) { // range of API pages (.length?)
+		//I limited the number of pages because if I request the API link many times, it drops the server
+	  $http.get('https://api.hypem.com/playlist/feed/lun0/json/' + pages)
+	  // Supposing that the request was about the user feed
+	  .then(function(data) {
+	    // Passing API object to array
+	    var inputObj = data.data; // object
+	    if (i == undefined) {
+	    	var i = 0; // just the 1st time
+	    }
+			for (var key in inputObj) {
+				// Taking out the "version" Object key of API page
+			  if (key < 40) { // length of each API page
+			  	output.push(inputObj[i]); // put inside of array
+			  }
+			  i++; // next index
+			}
+			// Requests
+			$scope.requests = output;
+			//consoling
+			//console.log('Input ---->', data.data);
+			//console.log('===========');
+			//console.log('Array ---->', output);
+			//console.log('===========');
+    	//console.log('Each Object ---->', output[0]);
+	  });
+	}
 });
